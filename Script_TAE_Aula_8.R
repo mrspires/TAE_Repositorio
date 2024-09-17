77d02ae5f6200011602769bcf880f0c5019f30a3
Márcia Roberta S P Silva <professora.marcia.pires@gmail.com>
2024-09-16 23:21:58 -0300

    Update and rename Script_TAE_Aula_10.R to Script_TAE_Aula_8.R

---
+++ Script_TAE_Aula_8.R
@@ -0,0 +1,39 @@
+# Tabela para as duas variveis de qualidade e uma de quantidade
+tabela <- with(milsa, ftable(Regiao, Est.civil, Inst))
+prop.table(tabela)
+
+# Medidas descritivas para múltiplas variáveis
+# Funçao aggregate é usada para agrupar dados pela combinação das categorias de duas ou mais variáveis qualitativas
+resumo_salario <- aggregate(Salario ~ Est.civil + Inst, data = milsa, FUN = summary)
+print(resumo_salario)
+
+# Gráfico a ser apresentado é o Boxplot pois queremos comparar as estatísticas da variável Salario nas categorias da variável Inst
+boxplot(Salario ~ Inst * Est.civil, data=milsa, main = "Salarios", xlab = "Nível de Instrução x Estado Civil")
+
+# Instalar os pacotes para visualização da matriz de correlação
+install.packages("corrplot")
+install.packages("ggcorrplot")
+library(corrplot)
+library(ggcorrplot)
+
+# Visualizar a matriz de correlação de três ou mais variáveis quantitativas
+matriz_correlacao <- with(milsa, ftable(Anos, Salario, Filhos))
+corrplot(matriz_correlacao, method = "circle")
+
+# Visualizar a matriz de correlação com ggcorrplot
+ggcorrplot(matriz_correlacao, lab = TRUE)
+
+# Instalar pacotes para calcular a medida de associação entre variáveis qualitativas
+install.packages("FactoMineR")
+install.packages("factoextra")
+
+library(FactoMineR)
+library(factoextra)
+
+# Realizar a MCA 
+resultado_mca <- MCA(Regiao + Est.civil + Inst, data = milsa, graph = FALSE)
+
+# Visualizar as variáveis 
+fviz_mca_biplot(resultado_mca, repel = TRUE, ggtheme = theme_minimal())
+ 
+
