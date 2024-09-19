4849764a3f2cf8db0a1520b4cdf8632f4e4a2ebb
Márcia Roberta S P Silva <professora.marcia.pires@gmail.com>
2024-09-19 09:06:18 -0300

    Update Script_TAE_Aula_9.R

--- Script_TAE_Aula_9.R
+++ Script_TAE_Aula_9.R
@@ -1,13 +1,22 @@
+#correlação das variáveis distancia e idade
+corr_simples <- cor(distancia$distancia, distancia$idade)
+corr_simples
+
+#correlação das variáveis VO2, IMC e carga
+multiplo <- data.frame(esteira$VO2, esteira$IMC, esteira$carga)
+cor(multiplo)
+
 # a função lm() ajusta o modelo de regressão linear
 modelo_simples <- lm(distancia ~ idade, data = distancia)
 modelo_simples
+
 
 modelo_multiplo <- lm(VO2 ~ IMC + carga, data = esteira)
 modelo_multiplo
 
 #Apresenta o gráfico de dispersão do modelo e a função linear plotada pela função abline()
 plot(distancia ~ idade, data = distancia, xlab = "Idade (em anos)", ylab = "Distancia (m)", main = "Gráfico de Dispersão")
-abline(modelo_simples, col = "red")
+abline(modelo_simples, col = "blue")
 
 
 # A função summary() apresenta um resumo estatístico do modelo
@@ -21,5 +30,6 @@ print(resumos)
 residuos_simples <- resumo$residuals
 plot(residuos_simples)
 
-residuos_multiplo <- resumos$residuals
-plot(residuaos_multiplos)
+residuos_multiplos <- resumos$residuals
+plot(residuos_multiplos)
+
