d3f876bdf24cdbbbde8fdf2f5d0bf728ad2cace6
Márcia Roberta S P Silva <professora.marcia.pires@gmail.com>
2024-09-18 21:02:24 -0300

    Update Script_TAE_Aula_8.R

--- Script_TAE_Aula_8.R
+++ Script_TAE_Aula_8.R
@@ -10,37 +10,53 @@ print(resumo_salario)
 # Gráfico a ser apresentado é o Boxplot pois queremos comparar as estatísticas da variável Salario nas categorias da variável Inst
 boxplot(Salario ~ Inst * Est.civil, data=milsa, main = "Salarios", xlab = "Nível de Instrução x Estado Civil")
 
-# Instalar os pacotes para visualização da matriz de correlação
-install.packages("corrplot")
-install.packages("ggcorrplot")
-library(corrplot)
-library(ggcorrplot)
 
 #Para calcular a média de uma variável que contém dados faltantes (ou ocultos como NA) em R,é necessário ignorar esses valores NA durante o cálculo. 
 #Isso pode ser feito utilizando o parâmetro na.rm = TRUE na função mean().
 valor_substituto <- mean(milsa$Filhos, na.rm = TRUE)
 
-#A função replace() é uma maneira simples de substituir valores NA
-data$variavel <- replace(milsa$Filhos, is.na(data$variavel), valor_substituto)
+#A função replace() é uma maneira simples de substituir valores NA na variável 
+milsa$Filhos <- replace(milsa$Filhos, is.na(milsa$Filhos), valor_substituto)
+
+# Instalar os pacotes para visualização da matriz de correlação
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("corrplot")
+library(corrplot)
+
+install.packages("ggcorrplot")
+library(ggcorrplot)
 
 # Visualizar a matriz de correlação de três ou mais variáveis quantitativas
 matriz_correlacao <- data.frame(milsa$Salario, milsa$Anos, milsa$Filhos)
-corrplot(matriz_correlacao, method = "circle")
+matriz <- cor(matriz_correlacao, use = "complete.obs")
+
+#Os método da função são para visualizar as correlações na matriz e são:
+# "circle", "square", "ellipse", "number", "shade", "color", "pie"
+corrplot(matriz, method = "circle")
 
 # Visualizar a matriz de correlação com ggcorrplot
-ggcorrplot(matriz_correlacao, lab = TRUE)
+ggcorrplot(matriz, lab = TRUE)
+
 
 # Instalar pacotes para calcular a medida de associação entre variáveis qualitativas
 install.packages("FactoMineR")
-install.packages("factoextra")
-
 library(FactoMineR)
+
+install.packages("factoextra")
 library(factoextra)
 
 # Realizar a MCA 
-resultado_mca <- MCA(Regiao + Est.civil + Inst, data = milsa, graph = FALSE)
+mca_variaveis <- data.frame(milsa$Regiao, milsa$Est.civil , milsa$Inst)
+resultado_mca <- MCA(mca_variaveis, graph = FALSE)
 
 # Visualizar as variáveis 
 fviz_mca_biplot(resultado_mca, repel = TRUE, ggtheme = theme_minimal())
- 
+
+#A MCA simplifica a análise de dados categóricos, revelando as relações entre categorias e observações.
+#Os eixos principais explicam as variações mais significativas.
+#A interpretação se dá pela proximidade, contribuição e qualidade de representação de categorias e indivíduos no gráfico.
+#Interpretar a MCA requer analisar tanto as categorias quanto as observações e como elas se relacionam com os eixos principais, 
+#buscando padrões ou agrupamentos importantes.
 
