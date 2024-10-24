2d751b17cb3522e9fa835e9288bd97cd7dd5ce72
Márcia Roberta S P Silva <professora.marcia.pires@gmail.com>
2024-10-24 08:10:47 -0300

    Update Script_TAE_Aula_14.R

--- Script_TAE_Aula_14.R
+++ Script_TAE_Aula_14.R
@@ -3,23 +3,23 @@ install.packages("caret")
 library(caret)
 
 # Usando a função glm() para obter os coeficientes da regressão logistica.
-dist_aberta <- glm(deslocamento ~ dist_aberta, family = binomial, data = disco)
+dist_aberta <- glm(deslocamento ~ distanciaA, family = binomial, data = disco)
 summary(dist_aberta)
 
-dist_fechada <- glm(deslocamento ~ dist_fechada, family = binomial, data = disco)
+dist_fechada <- glm(deslocamento ~ distanciaF, family = binomial, data = disco)
 summary(dist_fechada)
 
-ambas_dist <- glm(deslocamento ~ dist_aberta + dist_fechada, family = binomial, data = disco)
+ambas_dist <- glm(deslocamento ~ distanciaA + distanciaF, family = binomial, data = disco)
 summary(ambas_dist)
 
 #Ajuste do modelo de classificação por validação cruzada usando o método LOOCV
 #repetindo o método por 5 vezes
 train_control = trainControl(method= "LOOCV", number = 5, repeats = 5)
-modelo1 <- train(deslocamento ~ dist_aberta, data= disco, method= "glm", family= binomial, trControl=train_control)
+modelo1 <- train(deslocamento ~ distanciaA, data= disco, method= "glm", family= binomial, trControl=train_control)
 print(modelo1)
 
-modelo2 <- train(deslocamento ~ dist_fechada, data= disco, method= "glm", family= binomial, trControl=train_control)
+modelo2 <- train(deslocamento ~ distanciaF, data= disco, method= "glm", family= binomial, trControl=train_control)
 print(modelo2)
 
-modelo3 <- train(deslocamento ~ dist_aberta + dist_fechada, data= disco, method= "glm", family= binomial, trControl=train_control)
+modelo3 <- train(deslocamento ~ distanciaA + distanciaF, data= disco, method= "glm", family= binomial, trControl=train_control)
 print(modelo3)
