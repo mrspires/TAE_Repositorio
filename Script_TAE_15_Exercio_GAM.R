310a9da0bcde32b59cd669ce1e82d99ede2ca337
Márcia Roberta S P Silva <mrspires26@yahoo.com.br>
2025-04-29 07:53:04 -0300

    Add files via upload

---
+++ Script_TAE_15_Exercio_GAM.R
@@ -0,0 +1,5 @@
+# EXERCICIO DA AULA 15  - MODELOS ADITIVOS GENERALIZADOS
+# Dados arquivo esforco3 variáveis a serem criadas: IMC = Peso/(Altura/100)^2 - (a altura nos dados está em cm e na fórmula o cálculo é em metros)
+# e Superficie Corporea = sqrt(Altura*Peso/ 3600)
+
+
