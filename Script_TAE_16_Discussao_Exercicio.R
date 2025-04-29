2f93cbbf4525a4da805aca4fb1338977b4bf9248
Márcia Roberta S P Silva <professora.marcia.pires@gmail.com>
2025-04-29 11:32:05 -0300

    Update and rename Script_TAE_15_Exercicio_GAM.R to Script_TAE_16_Discussao_Exercicio.R

--- Script_TAE_16_Discussao_Exercicio.R
+++ Script_TAE_16_Discussao_Exercicio.R
@@ -1,4 +1,4 @@
-# EXERCICIO DA AULA 15  - MODELOS ADITIVOS GENERALIZADOS
+#DISCUSSÃO DAS TÉCNICAS LASSO, RIDGE, ELASTICNET E MODELOS ADITIVOS GENERALIZADOS
 
 # Dados arquivo esforco3 variáveis a serem criadas:  Indice de Massa Corporal, IMC = Peso/(Altura/100)^2 - (a altura nos dados está em cm e na fórmula o cálculo é em metros)
 # e Superficie Corporal, SC = sqrt(Altura*Peso/ 3600) ou ainda, fórmula de Du Bois, SC = 0.007184 * (Peso^0.425) * (Altura_cm^0.725)
