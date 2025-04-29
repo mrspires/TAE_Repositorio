f97dc76d65d8ae931af1eb85f2e49dcff3e7edbf
Márcia Roberta S P Silva <professora.marcia.pires@gmail.com>
2025-04-29 08:08:17 -0300

    Update Script_TAE_15_Exercicio_GAM.R

--- Script_TAE_15_Exercicio_GAM.R
+++ Script_TAE_15_Exercicio_GAM.R
@@ -1,8 +1,9 @@
 # EXERCICIO DA AULA 15  - MODELOS ADITIVOS GENERALIZADOS
 
-# Dados arquivo esforco3 variáveis a serem criadas: IMC = Peso/(Altura/100)^2 - (a altura nos dados está em cm e na fórmula o cálculo é em metros)
-# e Superficie Corporea = sqrt(Altura*Peso/ 3600)
+# Dados arquivo esforco3 variáveis a serem criadas:  Indice de Massa Corporal, IMC = Peso/(Altura/100)^2 - (a altura nos dados está em cm e na fórmula o cálculo é em metros)
+# e Superficie Corporal, SC = sqrt(Altura*Peso/ 3600) ou ainda, fórmula de Du Bois, SC = 0.007184 * (Peso^0.425) * (Altura_cm^0.725)
+
 
 esforco3$IMC <-  esforco3$peso/(esforco3$altura/100)^2
-esforco3$SupCor <- sqrt(esforco3$altura*esforco3$peso/ 3600)
-
+esforco3$SC <- sqrt(esforco3$altura*esforco3$peso/ 3600)
+esforco3$SC2 <-  0.007184 * (esfoco3$peso^0.425) * (esfoco3$altura_cm^0.725)
