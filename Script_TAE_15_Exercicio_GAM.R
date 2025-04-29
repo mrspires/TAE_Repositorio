17146abca440e482e4bc11d52b6babd0ee97f6a1
Márcia Roberta S P Silva <professora.marcia.pires@gmail.com>
2025-04-29 08:02:54 -0300

    Update Script_TAE_15_Exercicio_GAM.R

--- Script_TAE_15_Exercicio_GAM.R
+++ Script_TAE_15_Exercicio_GAM.R
@@ -1,5 +1,8 @@
 # EXERCICIO DA AULA 15  - MODELOS ADITIVOS GENERALIZADOS
+
 # Dados arquivo esforco3 variáveis a serem criadas: IMC = Peso/(Altura/100)^2 - (a altura nos dados está em cm e na fórmula o cálculo é em metros)
 # e Superficie Corporea = sqrt(Altura*Peso/ 3600)
 
+esforco3$IMC <-  esforco3$peso/(esforco3$altura/100)^2
+esforco3$SupCor <- sqrt(esforco3$altura*esforco3$peso/ 3600)
 
